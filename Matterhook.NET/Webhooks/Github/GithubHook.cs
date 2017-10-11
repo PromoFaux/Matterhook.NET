a74aedc7d59e6221df1623529a22166f9655af33
Adam Warner <adamw@rner.email>
2017-10-11 20:50:11 +0100

    Add another try/catch in. Probably wont do anything

--- Matterhook.NET/Webhooks/Github/GithubHook.cs
+++ Matterhook.NET/Webhooks/Github/GithubHook.cs
@@ -10,49 +10,57 @@ namespace Matterhook.NET.Webhooks.Github
 
       public GithubHook(StringValues strEvent, StringValues signature, StringValues delivery, string payloadText)
         {
-            Event = strEvent;
-            Signature = signature;
-            Delivery = delivery;
-            PayloadString = payloadText;
-
-            switch (Event)
+            try
             {
-                case "pull_request":
-                    Payload = JsonConvert.DeserializeObject<PullRequestEvent>(PayloadString);
-                    break;
-                case "issues":
-                    Payload = JsonConvert.DeserializeObject<IssuesEvent>(PayloadString);
-                    break;
-                case "issue_comment":
-                    Payload = JsonConvert.DeserializeObject<IssueCommentEvent>(PayloadString);
-                    break;
-                case "repository":
-                    Payload = JsonConvert.DeserializeObject<RepositoryEvent>(PayloadString);
-                    break;
-                case "create":
-                    Payload = JsonConvert.DeserializeObject<CreateEvent>(PayloadString);
-                    break;
-                case "delete":
-                    Payload = JsonConvert.DeserializeObject<DeleteEvent>(PayloadString);
-                    break;
-                case "pull_request_review":
-                    Payload = JsonConvert.DeserializeObject<PullRequestReviewEvent>(PayloadString);
-                    break;
-                case "pull_request_review_comment":
-                    Payload = JsonConvert.DeserializeObject<PullRequestReviewCommentEvent>(PayloadString);
-                    break;
-                case "push":
-                    Payload = JsonConvert.DeserializeObject<PushEvent>(PayloadString);
-                    break;
-                case "commit_comment":
-                    Payload = JsonConvert.DeserializeObject<CommitCommentEvent>(PayloadString);
-                    break;
-                case "status":
-                    Payload = JsonConvert.DeserializeObject<StatusEvent>(PayloadString);
-                    break;
-                default:
-                    throw new Exception($"Unhandled Event Type: {Event}");
+                Event = strEvent;
+                Signature = signature;
+                Delivery = delivery;
+                PayloadString = payloadText;
+
+                switch (Event)
+                {
+                    case "pull_request":
+                        Payload = JsonConvert.DeserializeObject<PullRequestEvent>(PayloadString);
+                        break;
+                    case "issues":
+                        Payload = JsonConvert.DeserializeObject<IssuesEvent>(PayloadString);
+                        break;
+                    case "issue_comment":
+                        Payload = JsonConvert.DeserializeObject<IssueCommentEvent>(PayloadString);
+                        break;
+                    case "repository":
+                        Payload = JsonConvert.DeserializeObject<RepositoryEvent>(PayloadString);
+                        break;
+                    case "create":
+                        Payload = JsonConvert.DeserializeObject<CreateEvent>(PayloadString);
+                        break;
+                    case "delete":
+                        Payload = JsonConvert.DeserializeObject<DeleteEvent>(PayloadString);
+                        break;
+                    case "pull_request_review":
+                        Payload = JsonConvert.DeserializeObject<PullRequestReviewEvent>(PayloadString);
+                        break;
+                    case "pull_request_review_comment":
+                        Payload = JsonConvert.DeserializeObject<PullRequestReviewCommentEvent>(PayloadString);
+                        break;
+                    case "push":
+                        Payload = JsonConvert.DeserializeObject<PushEvent>(PayloadString);
+                        break;
+                    case "commit_comment":
+                        Payload = JsonConvert.DeserializeObject<CommitCommentEvent>(PayloadString);
+                        break;
+                    case "status":
+                        Payload = JsonConvert.DeserializeObject<StatusEvent>(PayloadString);
+                        break;
+                    default:
+                        throw new Exception($"Unhandled Event Type: {Event}");
+                }
             }
+            catch (Exception e)
+            {
+                throw new Exception($"Problem Converting payload: {e.Message}");
+            }
+           
         }
 
         public string Event { get; set; }
