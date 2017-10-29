2315e6bd1d7bb16ae14243c69e3b8b44b44487b5
Adam Warner <adamw@rner.email>
2017-10-29 15:57:22 +0000

    don't post message to mattermost if Discourse post body is blank

--- Matterhook.NET/Controllers/DiscourseHookController.cs
+++ Matterhook.NET/Controllers/DiscourseHookController.cs
@@ -9,12 +9,14 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Matterhook.NET.Code;
-using Matterhook.NET.MatterhookClient;
 using Matterhook.NET.Webhooks.Discourse;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
 using Newtonsoft.Json.Linq;
 using ReverseMarkdown;
+using Matterhook.NET.MatterhookClient;
+
 
 namespace Matterhook.NET.Controllers
 {
@@ -47,11 +49,11 @@ namespace Matterhook.NET.Controllers
                 //Generate DiscourseHook object for easier reading
                 stuffToLog.Add($"Discourse Hook received: {DateTime.Now}");
 
-                Request.Headers.TryGetValue("X-Discourse-Event-Id", out var eventId);
-                Request.Headers.TryGetValue("X-Discourse-Event-Type", out var eventType);
-                Request.Headers.TryGetValue("X-Discourse-Event", out var eventName);
-                Request.Headers.TryGetValue("X-Discourse-Event-Signature", out var signature);
-                Request.Headers.TryGetValue("X-Discourse-Instance", out var discourseUrl);
+                Request.Headers.TryGetValue("X-Discourse-Event-Id", out StringValues eventId);
+                Request.Headers.TryGetValue("X-Discourse-Event-Type", out StringValues eventType);
+                Request.Headers.TryGetValue("X-Discourse-Event", out StringValues eventName);
+                Request.Headers.TryGetValue("X-Discourse-Event-Signature", out StringValues signature);
+                Request.Headers.TryGetValue("X-Discourse-Instance", out StringValues discourseUrl);
                 _discourseUrl = discourseUrl;
 
                 stuffToLog.Add($"Hook Id: {eventId}");
@@ -66,7 +68,7 @@ namespace Matterhook.NET.Controllers
 
                 if (signature == calcSig)
                 {
-                    var discourseHook = new DiscourseHook(eventId, eventType, eventName, signature, payloadText);
+                    var discourseHook = new DiscourseHook(eventId,eventType,eventName,signature,payloadText);
                     var matterHook = new MatterhookClient.MatterhookClient(_config.MattermostConfig.WebhookUrl);
                     HttpResponseMessage response = null;
                     MattermostMessage message = null;
@@ -82,20 +84,22 @@ namespace Matterhook.NET.Controllers
                             ? $"Unable to post to Mattermost {response.StatusCode}"
                             : "Unable to post to Mattermost");
 
-                        return Content(response != null
-                            ? $"Problem posting to Mattermost: {response.StatusCode}"
-                            : "Problem Posting to Mattermost");
+                        return Content(response != null ? $"Problem posting to Mattermost: {response.StatusCode}" : "Problem Posting to Mattermost");
                     }
                     if (message != null) stuffToLog.Add(message.Text);
                     stuffToLog.Add("Succesfully posted to Mattermost");
                     Util.LogList(stuffToLog);
                     return Ok();
                 }
-                stuffToLog.Add("Invalid Signature!");
-                stuffToLog.Add($"Expected: {signature}");
-                stuffToLog.Add($"Calculated: {calcSig}");
-                Util.LogList(stuffToLog);
-                return Unauthorized();
+                else
+                {
+                    stuffToLog.Add("Invalid Signature!");
+                    stuffToLog.Add($"Expected: {signature}");
+                    stuffToLog.Add($"Calculated: {calcSig}");
+                    Util.LogList(stuffToLog);
+                    return Unauthorized();
+                }
+        
             }
             catch (Exception e)
             {
@@ -103,6 +107,7 @@ namespace Matterhook.NET.Controllers
                 Util.LogList(stuffToLog);
                 return Content(e.Message);
             }
+           
         }
 
         private static string ExpandDiscourseUrls(string input, string discourseUrl)
@@ -117,9 +122,14 @@ namespace Matterhook.NET.Controllers
             var p = payload.post;
 
             if (_config.IgnoredTopicTitles.Contains(p.topic_title))
+            {
                 throw new Exception("Post title matches ignored titles");
+            }
 
             if (_config.IgnorePrivateMessages)
+            {
+                //We're not really doing anything here yet
+                //TODO: Revisit in the future
                 try
                 {
                     JObject.Parse(
@@ -129,6 +139,12 @@ namespace Matterhook.NET.Controllers
                 {
                     throw new Exception("Unable to retrieve topic, possibly a PM so we should ignore this.");
                 }
+            }
+
+            if (p.cooked == "")
+            {
+                throw new Exception("Body empty, nothing to post.");
+            }
 
 
             var retVal = new MattermostMessage
@@ -145,20 +161,25 @@ namespace Matterhook.NET.Controllers
                         Fallback = "New Post in Discourse Topic",
                         Title = p.topic_title,
                         TitleLink = new Uri($"{_discourseUrl}/t/{p.topic_id}/{p.post_number}"),
-                        Text = new Converter().Convert(ExpandDiscourseUrls(p.cooked, _discourseUrl)),
+                        Text = new Converter().Convert(ExpandDiscourseUrls(p.cooked,_discourseUrl)),
                         AuthorName = p.username,
                         AuthorLink = new Uri($"{_discourseUrl}/u/{p.username}"),
-                        AuthorIcon = new Uri($"{_discourseUrl}{p.avatar_template.Replace("{size}", "16")}")
+                        AuthorIcon = new Uri($"{_discourseUrl}{p.avatar_template.Replace("{size}","16")}")
                     }
                 }
+
             };
 
             if (p.post_number.ToString() == "1")
+            {
                 retVal.Text = "#NewTopic\n";
+            }
 
             retVal.Text += $"#{p.topic_slug}";
 
             return retVal;
         }
+
+
     }
 }
